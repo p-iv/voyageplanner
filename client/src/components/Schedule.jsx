a35f5d75295cba9801b29792bdaaa444f3a7f0bd
p-iv <pavlo.iv00@gmail.com>
2024-07-20 21:09:51 +0200

    created  schedule component

--- client/src/components/Schedule.jsx
+++ client/src/components/Schedule.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
 import styles from "./Schedule.module.css";
+import { Button, DatePicker, Space } from "antd";
+const { RangePicker } = DatePicker;
 
 function Schedule() {
-  return <div>schedule</div>;
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <div className={styles.schedule}>
+      <Space direction="vertical" size={12}>
+        <RangePicker
+          className={styles.datePicker}
+          showTime={{
+            format: "HH:mm",
+          }}
+          format="YYYY-MM-DD HH:mm"
+          onChange={(newDate) => {
+            console.log(newDate);
+            setDate(newDate);
+          }}
+        />
+      </Space>
+      <Button type="primary">Choose Date</Button>
+    </div>
+  );
 }
 
 export default Schedule;
